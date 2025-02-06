0e6b62a7bf399e21a90612c44c89c8b7848c0b87
Bruno Costa Dourado <brunoc.dourado10@gmail.com>
2025-02-06 11:07:48 -0300

    Commit Aula POO 06/02

--- src/main/java/com/senai/aula01_introducao_poo/exemplos/pessoa/Main.java
+++ src/main/java/com/senai/aula01_introducao_poo/exemplos/pessoa/Main.java
@@ -1,7 +1,10 @@
 package com.senai.aula01_introducao_poo.exemplos.pessoa;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Pessoa pessoa1 = new Pessoa(
                 "Rafael",
                 18,
@@ -18,6 +21,30 @@ public class Main {
         pessoa1.falar("Alôôôôôôôô");
         pessoa1.comer("Arroz");
         System.out.println(pessoa1);
+
+        String[] etiqueta = {"Nome: ", "Idade: ", "Altura: ", "Endereço: \n\t Logradouro: ", "\t Nome do endereço: ", "\t Número: ", "\t Bairro: ", "\t Cidade: ", "\t Estado: "};
+        String[] dados = new String[etiqueta.length];
+
+        System.out.println("Preencha os dados a seguir: ");
+        for (int i = 0; i < etiqueta.length; i++) {
+            System.out.print(etiqueta[i]);
+            dados[i] = scanner.nextLine();
+        }
+
+        Pessoa pessoa2 = new Pessoa(
+                dados[0],
+                Integer.parseInt(dados[1]),
+                Float.parseFloat(dados[2]),
+                new Endereco(
+                        dados[3],
+                        dados[4],
+                        dados[5],
+                        dados[6],
+                        dados[7],
+                        dados[8]
+                )
+
+        );
 
     }
 }
